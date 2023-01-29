e2322ac1e7afdafa3538a2b6b71e56a553f6ad5e
Admin <Admin@ADMIN>
2023-01-29 20:00:32 +0300

    encoding

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 using namespace std;
 
-void main()
+int main()
 {
 	setlocale(LC_ALL, "Rus");
 
 	const int n = 5;
 	double sum = 0;// переменная для суммирования 
 	int arr[n] ;
+	int max = arr[0];//переменнаяя для максимального значения
+	unsigned int min = arr[0];//переменаня для минимального значения, положтельное значение 
 	for (int i = 0; i < n; i++)
 	{
 		cout << "Ввидите значение массива : ";
 		cin >> arr[i];
 	}
-	//Вывод массива на экран в прямом прорядке :
+	cout << endl;
+
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 		sum += arr[i];
+		if (max < arr[i])
+			max = arr[i];
+		if (min > arr[i])
+			min = arr[i];
 	}
-	cout << "\n\n Сумма = " << sum;
-	cout << endl;
-	cout << "\n Среднее значение массива = " << sum / n << endl;
-
+	cout << "\n\nСумма = " << sum << endl;
+	cout << "\nСреднее значение массива = " << sum / n << endl;
+	cout << "\nМаксимальное значение массива = " << max << endl;
+	cout << "\nМинимальное значение массива = " << min << endl;
 
 	//Вывод массива на экран в обратном порядке: 
 
